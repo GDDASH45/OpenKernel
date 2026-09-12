6bc90248824aa3d88426a80261e7a61d4fd00520
PuppyDog <dawgcookie28@gmail.com>
2026-09-12 17:54:34 -0400

    trying to fix a module freeze

---
+++ include/ksys/system-info.h
@@ -0,0 +1,13 @@
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+// Codename
+#define CODENAME "Chocolate Ice Cream"
+
+// Release
+#define RELEASE "In Development"
+
+// Github repository
+#define GITHUB_REPO "https://github.com/GDDASH45/OpenKernel"
+
+#endif
--- modules/info.c
+++ modules/info.c
@@ -1,6 +1,7 @@
 #include <write/write.h>
 #include <ksys/version.h>
 #include <kernel/time.h>
+#include <ksys/system-info.h>
 
 void info_module_init(void) {
     k_print("----------------------------------------\n");
@@ -11,4 +12,22 @@ void info_module_init(void) {
     k_print("----------------------------------------\n");
     k_print("Kernel version: ");
     k_print(K_VER);
+    k_print("----------------------------------------\n");
+    //sleep_ms(2000);
+    //k_clear_screen();
+    k_print("----------------------------------------\n");
+    k_print("Codename: ");
+    k_print(CODENAME);
+    k_print("----------------------------------------\n");
+    sleep_ms(2000);
+    k_print("----------------------------------------\n");
+    k_print("Release: ");
+    k_print(RELEASE);
+    k_print("----------------------------------------\n");
+    sleep_ms(2000);
+    k_clear_screen();
+    k_print("----------------------------------------\n");
+    k_print("Check out the kernel source at: ");
+    k_print(GITHUB_REPO);
+    k_print("----------------------------------------\n");
 }
