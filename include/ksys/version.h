89c8d63dbbe897be32aa2a887a29d8fca3811a8b
PuppyDog <dawgcookie28@gmail.com>
2026-09-12 17:32:29 -0400

    Add ksys/version.h and updated info.c to use the K_VER define

---
+++ include/ksys/version.h
@@ -0,0 +1,6 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define K_VER "0.0.1 In Development"
+
+#endif
--- modules/info.c
+++ modules/info.c
@@ -1,8 +1,13 @@
 #include <write/write.h>
+#include <ksys/version.h>
 
 void info_module_init(void) {
     k_print("----------------------------------------\n");
     k_print("[MODULE] Loaded: System Information\n");
     k_print("[MODULE] OpenKernel extension active.\n");
     k_print("----------------------------------------\n");
+    k_clear_screen();
+    k_print("----------------------------------------\n");
+    k_print("Kernel version: ");
+    k_print(K_VER);
 }
