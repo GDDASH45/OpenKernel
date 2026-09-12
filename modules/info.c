e0d8f7395dad89b6d9431f077178b5b81ef5a64e
PuppyDog <dawgcookie28@gmail.com>
2026-09-12 17:43:51 -0400

    Use the sleep_ms in the info module

--- modules/info.c
+++ modules/info.c
@@ -1,12 +1,13 @@
 #include <write/write.h>
 #include <ksys/version.h>
+#include <kernel/time.h>
 
 void info_module_init(void) {
     k_print("----------------------------------------\n");
     k_print("[MODULE] Loaded: System Information\n");
     k_print("[MODULE] OpenKernel extension active.\n");
     k_print("----------------------------------------\n");
-    k_clear_screen();
+    sleep_ms(3000);
     k_print("----------------------------------------\n");
     k_print("Kernel version: ");
     k_print(K_VER);
